d663dd97d0c5ef60fb7e113538bbd2ef548fa4f8
Thiago Caetano Dantas <127804127+VirtuousThigas@users.noreply.github.com>
2024-08-01 16:59:01 -0300

    Add files via upload

---
+++ Questão 5/Questão 5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a Frase:");
+            string frase = Console.ReadLine();
+            frase = frase.ToLower();
+            string[] palavras = frase.Split(' ');
+            Dictionary<string, int> frutas = new Dictionary<string, int>();
+            foreach (string palavra in palavras)
+            {
+                if (frutas.ContainsKey(palavra))
+                {
+                    frutas[palavra]++;
+                }
+                else
+                {
+                    frutas[palavra] = 1;
+                }
+            }
+            foreach (var par in frutas)
+            {
+                Console.WriteLine($"{par.Key}: {par.Value} ocorrencia(s).");
+            }
+        }
+    }
+}
